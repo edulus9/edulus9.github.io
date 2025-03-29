a4c77974e381c00449e83d73aea21aa7bdfc9281
Edoardo <edulus9@gmail.com>
2025-03-29 19:05:48 +0100

    feat: added basic i18n support

--- src/appInit.tsx
+++ src/appInit.tsx
@@ -3,17 +3,23 @@ import { createRoot } from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
 import defaultTheme from 'react95/dist/themes/original';
 import { ThemeProvider } from 'styled-components';
+import { initializeI18N } from 'utils/initializeI18N';
 import App from './App';
 
-const container = document.getElementById('app');
-const root = createRoot(container!);
+const renderApp = () => {
+  const container = document.getElementById('app');
+  const root = createRoot(container!);
+  initializeI18N();
 
-root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={defaultTheme}>
-      <Router>
-        <App />
-      </Router>
-    </ThemeProvider>
-  </React.StrictMode>
-);
+  root.render(
+    <React.StrictMode>
+      <ThemeProvider theme={defaultTheme}>
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+};
+
+renderApp();
---
+++ src/locales/en_US.ts
@@ -0,0 +1,5 @@
+export const en_US = {
+  translation: {
+    start: 'Start!',
+  },
+};
