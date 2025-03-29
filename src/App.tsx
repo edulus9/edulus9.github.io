6153d5e2c2577bb45c045126719b4803088af4f2
Edoardo <edulus9@gmail.com>
2025-03-29 18:40:45 +0100

    feat: ran ze linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-
 import {} from '@react95/icons';
 import { TopNavBar } from './components/TopNavBar';
 
 export interface AppProps {}
 
 const App: React.FC<AppProps> = () => {
-    return <TopNavBar />;
+  return <TopNavBar />;
 };
 
 export default App;
